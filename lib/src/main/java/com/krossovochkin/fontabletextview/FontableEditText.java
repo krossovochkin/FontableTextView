9d1cd28c0b36cffe9744cf31d83f8baf5ea1e5dd
unknown <v.drobushkov@vdrobushkov.ocsico.local>
2014-05-30 17:53:13 +0300

    added license

--- lib/src/main/java/com/krossovochkin/fontabletextview/FontableEditText.java
+++ lib/src/main/java/com/krossovochkin/fontabletextview/FontableEditText.java
@@ -1,3 +1,19 @@
+/*
+* Copyright 2014 Vasya Drobushkov
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package com.krossovochkin.fontabletextview;
 
 import android.content.Context;
@@ -6,9 +22,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.EditText;
 
-/**
- * Created by v.drobushkov on 5/30/14.
- */
 public class FontableEditText extends EditText {
 
     public FontableEditText(Context context) {
--- lib/src/main/java/com/krossovochkin/fontabletextview/FontableTextView.java
+++ lib/src/main/java/com/krossovochkin/fontabletextview/FontableTextView.java
@@ -1,8 +1,20 @@
-package com.krossovochkin.fontabletextview;
+/*
+* Copyright 2014 Vasya Drobushkov
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
-/**
- * Created by v.drobushkov on 5/30/14.
- */
+package com.krossovochkin.fontabletextview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -10,9 +22,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.TextView;
 
-/**
- * Created by v.drobushkov on 5/28/14.
- */
 public class FontableTextView extends TextView {
 
     public FontableTextView(Context context) {
